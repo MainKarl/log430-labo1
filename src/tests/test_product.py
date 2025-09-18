662626a84f3bf3f845ece81e756ec52bd4d3030b
Karl Mainville <karlmainville26@gmail.com>
2025-09-18 10:52:58 -0400

    changing yml

--- src/tests/test_product.py
+++ src/tests/test_product.py
@@ -5,10 +5,14 @@ dao = ProductDAO()
 
 def test_product_select():
     product_list = dao.select_all()
-    assert 
+    assert len(product_list) >= 3
 
 def test_product_insert():
-    assert "Le test n'est pas encore là" == 1
+    product = Product(None, 'Test', 'Test_brand', 100.0)
+    dao.insert(product)
+    product_list = dao.select_all()
+    names = [p.name for p in product_list]
+    assert product.name in names
 
 def test_product_update():
     assert "Le test n'est pas encore là" == 1
